7921cff51d2455b67cb5afc4174dc2a4e25421a7
Kumari <anjali.kumari1@clarivate.com>
2021-07-19 12:58:50 +0530

    new files added

---
+++ oopsCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace oopsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Modifiers obj1 = new Modifiers("anjali");
+            
+            Console.WriteLine(obj1.data);
+            obj1.Data1 = "new value";
+            Console.WriteLine(obj1.Data1);
+            Child obj2 = new Child("data1", " data2");
+            Console.WriteLine(obj2.data);
+
+
+
+        }
+    }
+}
---
+++ oopsCSharp/modifiers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oopsCSharp
+{
+    class Modifiers
+    {
+        public string data;
+
+        public string Data1 //automatic properties(shorthand) => no ne
+        { get; set; }
+
+        //public string Data1
+        //{
+        //    get { return data1; }
+        //    set { data1 = value; }
+        //}
+
+
+        public Modifiers(string dataval)
+        {
+            data = dataval;
+        }
+
+
+    }
+
+    class Child : Modifiers
+    {
+
+        public string data1;
+
+        public Child(string b , string d ) : base(d)
+        {
+            data1 = b;
+        }
+
+
+       
+       
+    }
+
+
+}
