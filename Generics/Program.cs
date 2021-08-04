49d24847961c5eb4bc99feaa7ad74cac8d10024f
Kumari <anjali.kumari1@clarivate.com>
2021-08-04 16:23:18 +0530

    changes made

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -2,11 +2,41 @@
 
 namespace Generics
 {
+
+
+
+    public class Common<T> {
+
+        private T data;
+        
+        public T value
+        {
+
+            // using accessors
+            get
+            {
+                return this.data;
+            }
+            set
+            {
+                this.data = value;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Common<string> obj1 = new Common<string>();
+            obj1.value = "test data";
+            Common<int> obj2 = new Common<int>();
+            obj2.value = 34;
+            Console.WriteLine(obj1.value);
+            Console.WriteLine(obj2.value);
+            Console.ReadLine();
+
+
         }
     }
 }
