783d13efee08d7b6b65e558b4596063466cf5780
Kumari <ianjali1598@gmail.com>
2021-08-20 16:59:15 +0530

    changes made

---
+++ UnsafeCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UnsafeCode
+{
+    class Program
+    {
+
+        static int Square(int n)
+        {
+            return n * n;
+        }
+
+        static unsafe void Square(int* pn)
+        {
+            *pn = *pn * *pn;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            n = 10;
+            unsafe
+            {
+                Square(&n);
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
